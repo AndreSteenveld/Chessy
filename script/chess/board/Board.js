6a90ea2b14a76694c9bbf08c3d1cd9826f04eeb2
Andre Steenveld <andre.steenveld@gmai.com>
2013-01-17 18:42:57 +0100

    Added a debug toString() method to the board.

--- script/chess/board/Board.js
+++ script/chess/board/Board.js
@@ -157,6 +157,55 @@ define([
 			
 			newPiece.place( this, field );
 			
+		},
+		
+		toString: function( ){
+			
+			var BOARD_TEMPLATE = "\n"
+				+ "\n   +-+-+-+-+-+-+-+-+ "
+				+ "\n 8 |${a8}|${b8}|${c8}|${d8}|${e8}|${f8}|${g8}|${h8}| "
+				+ "\n   +-+-+-+-+-+-+-+-+ "
+				+ "\n 7 |${a7}|${b7}|${c7}|${d7}|${e7}|${f7}|${g7}|${h7}| "
+				+ "\n   +-+-+-+-+-+-+-+-+ "
+				+ "\n 6 |${a6}|${b6}|${c6}|${d6}|${e6}|${f6}|${g6}|${h6}| "
+				+ "\n   +-+-+-+-+-+-+-+-+ "
+				+ "\n 5 |${a5}|${b5}|${c5}|${d5}|${e5}|${f5}|${g5}|${h5}| "
+				+ "\n   +-+-+-+-+-+-+-+-+ "
+				+ "\n 4 |${a4}|${b4}|${c4}|${d4}|${e4}|${f4}|${g4}|${h4}| "
+				+ "\n   +-+-+-+-+-+-+-+-+ "
+				+ "\n 3 |${a3}|${b3}|${c3}|${d3}|${e3}|${f3}|${g3}|${h3}| "
+				+ "\n   +-+-+-+-+-+-+-+-+ "
+				+ "\n 2 |${a2}|${b2}|${c2}|${d2}|${e2}|${f2}|${g2}|${h2}| "
+				+ "\n   +-+-+-+-+-+-+-+-+ "
+				+ "\n 1 |${a1}|${b1}|${c1}|${d1}|${e1}|${f1}|${g1}|${h1}| "
+				+ "\n   +-+-+-+-+-+-+-+-+ "
+				+ "\n    A B C D E F G H  "
+				+ "\n Capital letters are white "
+				+ "\n";
+
+			return lib.string.substitute( BOARD_TEMPLATE, this.fields, 
+				function( value, key ){
+					
+					if( value.piece ){
+					
+						switch( value.piece.type ){
+													
+							case "Bishop": return value.piece.color === "white" ? "B" : "b";
+							case "King":   return value.piece.color === "white" ? "X" : "x";
+							case "Knight": return value.piece.color === "white" ? "K" : "k";
+							case "Pawn":   return value.piece.color === "white" ? "P" : "p";
+							case "Queen":  return value.piece.color === "white" ? "Q" : "q";
+							case "Rook":   return value.piece.color === "white" ? "R" : "r";
+							
+						}
+						
+					} else {
+						
+						return " ";	
+						
+					}			
+				}
+			);
 		}
 	});
 	
